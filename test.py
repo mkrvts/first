130667b887356d9f5d1be6ce3499fb8743d58cb8
Marina Kryvets <mkvts@Marinas-MBP.lan>
2019-02-08 17:00:56 +0100

    edited test

--- test.py
+++ test.py
@@ -1,39 +1,38 @@
 import unittest
 from selenium import webdriver
 
+
 class BlocketAuthorization(unittest.TestCase):
 
-    # creating instance
     def setUp(self):
         self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(30)
 
-    def blocket_authorization(self):
-        driver = self.driver
-
+    def test_google(self):
         # step 1 - open home page
-        driver.get("https:/blocket.se")
+        self.driver.get("https:/blocket.se")
 
         # step 2 - accept data agregation
-        driver.switch_to.frame('ufti-iframe')
-        accept_data_tracking = driver.find_element_by_xpath('//span[contains(text(), "Okej, tack!")]').click()
+        self.driver.switch_to.frame('ufti-iframe')
+        self.driver.find_element_by_xpath('//span[contains(text(), "Okej, tack!")]').click()
 
         # step 3 - go to login page
-        login_page_link = driver.find_element_by_xpath('//*[@id="top_nav_mypages"]/a').click()
+        self.driver.find_element_by_xpath('//*[@id="top_nav_mypages"]/a').click()
 
         # step 3.1 - step 4 doesn't work without wait
-        driver.implicitly_wait(5)
+        self.driver.implicitly_wait(5)
 
         # step 4 - fill in and submit authorization form
-        email_field = driver.find_element_by_xpath('//input[@type="email"]').send_keys("krvts.m@gmail.com")
-        password_field = driver.find_element_by_xpath('//*[@id="CredPassword"]').send_keys("qwaszx21")
+        self.driver.find_element_by_xpath('//input[@type="email"]').send_keys("krvts.m@gmail.com")
+        self.driver.find_element_by_xpath('//*[@id="CredPassword"]').send_keys("qwaszx21")
 
-        login_button = driver.find_element_by_xpath('//button[@type="submit"]').click()
+        self.driver.find_element_by_xpath('//button[@type="submit"]').click()
 
-        assert "krvts_m" not in driver.page_source
+        # assert "krvts_m" not in self.driver.page_source
 
-    def tearDoun(self):
-        self.driver.close()
+    def tearDown(self):
+        self.driver.quit()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
